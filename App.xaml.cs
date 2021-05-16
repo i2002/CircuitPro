e1340472f5e0f46b0d4cd4c0809919a32f280997
Tudor Butufei <tudor.butufei@gmail.com>
2021-05-16 16:06:44 +0300

    implementare gestiune fisiere

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+
+using CircuitPro.CircuitModel;
 
 namespace CircuitPro
 {
@@ -14,9 +14,114 @@ namespace CircuitPro
     public partial class App : Application
     {
         public CircuitModel.Circuit circuit;
-        private void Application_Startup(object sender, StartupEventArgs e)
+        private bool modificat = false;
+        private string numeFisier = "";
+
+        private static readonly string filters = "Diagramă circuit|*.cpro|Toate fișierele|*.*";
+
+
+        // ------------- Operatii ---------------
+        public void DocumentNou()
         {
-            circuit = new CircuitModel.Circuit();
+            if(InchidereFisier())
+            {
+                SetareCircuit();
+            }
+        }
+
+        public void Salvare()
+        {
+            if(numeFisier == "")
+            {
+                // setari dialog
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Title = "Salvează circuit",
+                    FileName = "Circuit nou",
+                    DefaultExt = ".cpro",
+                    Filter = filters
+                };
+
+                // afisare dialog
+                if (saveFileDialog.ShowDialog() != true)
+                {
+                    return;
+                }
+
+                // selectare nume fisier
+                numeFisier = saveFileDialog.FileName;
+            }
+
+            // salvare circuit
+            string json = circuit.Serialize();
+            File.WriteAllText(numeFisier, json);
+            SetModificat(false);
+        }
+
+        public void Deschidere()
+        {
+            if(!InchidereFisier())
+            {
+                return;
+            }
+
+            // setari dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog { Title = "Deschide circuit", Filter = filters };
+
+            // afisare dialog
+            if(openFileDialog.ShowDialog() == true)
+            {
+                // deschidere circuit
+                string json = File.ReadAllText(openFileDialog.FileName);
+                Circuit c = Circuit.Deserialize(json);
+                SetareCircuit(c, openFileDialog.FileName);
+            }
+        }
+
+        public bool InchidereFisier()
+        {
+            if (modificat)
+            {
+                MessageBoxResult res = MessageBox.Show("Doriți să salvați modificările", "Închidere fișier", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning, MessageBoxResult.Yes);
+                if(res == MessageBoxResult.Yes)
+                {
+                    Salvare();
+                    return true;
+                }
+
+                return res == MessageBoxResult.No;
+            }
+
+            return true;
+        }
+
+        public void SetModificat(bool mod = true)
+        {
+            modificat = mod;
+            ActualizareTitlu();
+        }
+
+
+        // ----------- Gestiune date ------------
+        private void SetareCircuit(Circuit c = null, string fileName = "")
+        {
+            // actualizare model
+            circuit = c ?? new Circuit();
+            numeFisier = fileName;
+            modificat = false;
+
+            // actualizare view
+            ((MainWindow)MainWindow).UpdateCircuit();
+            ActualizareTitlu();
+        }
+
+        private void ActualizareTitlu()
+        {
+            string aplicatie = "Circuit Pro";
+            string fisier = numeFisier == "" ? "Circuit nou" : Path.GetFileName(numeFisier);
+            string decorator = modificat ? "* " : "";
+
+            MainWindow.Title = $"{decorator}{fisier} - {aplicatie}";
         }
     }
 }
